e620c56720da48ff1987abdfdb828ee8ec3b752d
Curstantine <curstantine@proton.me>
2025-02-21 19:54:02 +0530

    feat: error fields

--- src/app/_components/form/Input.tsx
+++ src/app/_components/form/Input.tsx
@@ -2,14 +2,22 @@ import type { InputHTMLAttributes } from "react";
 
 import Label from "~/app/_components/form/Label";
 
-type Props = InputHTMLAttributes<HTMLInputElement> & {
+export type Props = InputHTMLAttributes<HTMLInputElement> & {
 	id: string;
 	label: string;
 };
 
-export default function Input({ id, label, disabled, ...rest }: Props) {
+export default function Input(props: Props) {
 	return (
 		<div className="inline-flex flex-col gap-1">
+			<InputInner {...props} />
+		</div>
+	);
+}
+
+export function InputInner({ id, label, disabled, ...rest }: Props) {
+	return (
+		<>
 			<Label htmlFor={id} aria-disabled={disabled}>
 				{label}
 			</Label>
@@ -19,6 +27,6 @@ export default function Input({ id, label, disabled, ...rest }: Props) {
 				disabled={disabled}
 				className="border-border transition-colors_opacity focus-visible:border-input h-9 rounded-md border bg-transparent px-2 text-sm focus-visible:outline-hidden disabled:opacity-50"
 			/>
-		</div>
+		</>
 	);
 }
