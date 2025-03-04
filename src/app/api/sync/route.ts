8fc69215e82ff80d45fed134aa6ca84a733549ee
Curstantine <curstantine@proton.me>
2025-03-04 12:48:21 +0530

    feat: check for latest articles, filter only by title, move to correct dirs, fix api routing and clean up

---
+++ src/app/api/sync/route.ts
@@ -0,0 +1,78 @@
+import { stat, copyFile, mkdir } from "node:fs/promises";
+import { join as pathJoin } from "node:path";
+
+import { NextResponse } from "next/server";
+import simpleGit from "simple-git";
+
+import { isMostlyEnglish, readMetadata } from "~/app/api/sync/utils";
+import { db } from "~/server/db";
+
+export const runtime = "nodejs";
+
+const repoOwner = "nuuuwan";
+const repoName = "news_long_lk";
+
+const sourcePath = pathJoin("news_source_data");
+const sourceDataPath = pathJoin(sourcePath, "data");
+const targetPath = pathJoin("news_filtered_data");
+
+const log = (msg: string) => console.log(`[sync] ${msg}`);
+
+export async function POST() {
+	try {
+		await cloneOrReuse();
+	} catch (e) {
+		console.error("Failed to clone or reuse repository: ", e);
+		return NextResponse.json({ status: "error" });
+	}
+
+	const metadata = await readMetadata(sourceDataPath);
+	const lastDBUpdate = await db.configuration.findUnique({
+		where: { key: "lastDBUpdate" },
+		select: { value: true },
+	});
+
+	const lastUpdate = Number(lastDBUpdate?.value || 0);
+	const newArticles = metadata.filter((x) => isMostlyEnglish(x.title) && x.ut > lastUpdate);
+
+	log(`Found ${newArticles.length} new articles`);
+	if (newArticles.length === 0) return NextResponse.json({ status: "ok" });
+
+	for (let i = 0; i < newArticles.length; i++) {
+		const meta = newArticles.at(i);
+		if (!meta) continue;
+
+		const articlePath = pathJoin(sourcePath, meta.dir_path_unix);
+		const folderName = articlePath.split("/").at(-1) || "not-found" + i;
+
+		const targetFolder = pathJoin(targetPath, folderName);
+		await mkdir(targetFolder, { recursive: true });
+		await copyFile(
+			pathJoin(articlePath, "article.json"),
+			pathJoin(targetFolder, "article.json"),
+		);
+	}
+
+	return NextResponse.json({ status: "ok" });
+}
+
+async function cloneOrReuse() {
+	const git = simpleGit();
+	const dirExists = await stat(sourcePath)
+		.then((e) => e.isDirectory())
+		.catch(() => false);
+
+	if (dirExists) {
+		log("Pulling latest changes...");
+
+		await git.cwd(sourcePath).pull("origin", "main");
+		log("Repository updated successfully!");
+
+		return NextResponse.json({ status: "ok" });
+	}
+
+	log("Cloning repository...");
+	await git.clone(`https://github.com/${repoOwner}/${repoName}.git`, sourcePath, { depth: 1 });
+
+	log("Repository cloned successfully!");
+}
---
+++ src/app/api/sync/types.ts
@@ -0,0 +1,7 @@
+export interface SourceArticleMetadata {
+	title: string;
+	url: string;
+	ut: number;
+	dir_path_unix: string;
+	time_str_formatted: string;
+}
