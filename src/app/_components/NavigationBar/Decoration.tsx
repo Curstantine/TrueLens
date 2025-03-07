40922188becdc1db0ab208095331c3546561a41b
Curstantine <curstantine@proton.me>
2025-03-07 11:58:37 +0530

    feat: navigation border on scroll

---
+++ src/app/_components/NavigationBar/Decoration.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+
+export default function NavBarDecoration() {
+	const [isUnder, under] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 0) {
+				under(true);
+			} else {
+				under(false);
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
+	return (
+		<div
+			className={clsx(
+				"absolute top-14 z-50 h-px w-full transition-colors duration-emphasized-decelerate ease-emphasized-decelerate",
+				isUnder && "bg-border",
+			)}
+		/>
+	);
+}
