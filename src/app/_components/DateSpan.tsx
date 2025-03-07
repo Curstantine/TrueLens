6a1cb67ef9c0ff354564a17656a6d0dd1bdcfd70
Curstantine <curstantine@proton.me>
2025-03-07 11:51:06 +0530

    feat: add user comments design

--- src/app/_components/DateSpan.tsx
+++ src/app/_components/DateSpan.tsx
@@ -2,20 +2,32 @@
 
 type Props = { value: Date; className?: string };
 
-const formatter = new Intl.DateTimeFormat("en-US", {
+const dateFormatter = new Intl.DateTimeFormat("en-US", {
 	year: "numeric",
 	month: "long",
 	day: "numeric",
 });
 
-const relativeFormatter = new Intl.RelativeTimeFormat("en", {
+const dateTimeFormatter = new Intl.DateTimeFormat("en-US", {
+	year: "numeric",
+	month: "long",
+	day: "numeric",
+	hour: "numeric",
+	minute: "numeric",
+});
+
+const relFormatter = new Intl.RelativeTimeFormat("en", {
 	localeMatcher: "best fit",
 	numeric: "auto",
 	style: "long",
 });
 
 export function DateSpan({ value, className }: Props) {
-	return <span className={className}>{formatter.format(value)}</span>;
+	return <span className={className}>{dateFormatter.format(value)}</span>;
+}
+
+export function DateTimeSpan({ value, className }: Props) {
+	return <span className={className}>{dateTimeFormatter.format(value)}</span>;
 }
 
 export function RelativeDateSpan({ value, className }: Props) {
@@ -23,8 +35,8 @@ export function RelativeDateSpan({ value, className }: Props) {
 	const diff = value.getTime() - now.getTime();
 
 	return (
-		<span className={className} title={formatter.format(value)}>
-			{relativeFormatter.format(Math.round(diff / (1000 * 60 * 60)), "day")}
+		<span className={className} title={dateFormatter.format(value)}>
+			{relFormatter.format(Math.round(diff / (1000 * 60 * 60)), "day")}
 		</span>
 	);
 }
