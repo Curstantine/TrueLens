bc103a959aa9089d0f05ff3f3a37c64ef1e337eb
Curstantine <curstantine@proton.me>
2025-03-10 18:12:43 +0530

    fix: set min_topic_size and remove nr

--- src/app/api/sync/grouping.py
+++ src/app/api/sync/grouping.py
@@ -77,27 +77,26 @@ def cluster_articles(articles):
         raise ValueError("No valid articles found for clustering.")
 
     model = SentenceTransformer("paraphrase-MiniLM-L12-v2")
-
     topic_model = BERTopic(
         language="english",
-        calculate_probabilities=True,
         verbose=True,
         embedding_model=model,
-        nr_topics=64,
+        min_topic_size=20,
     )
-    topics, probabilities = topic_model.fit_transform(body_paragraphs)
+    topics = topic_model.fit_transform(body_paragraphs)
     topic_model.save(MODEL_DIR, serialization="safetensors", save_ctfidf=True)
 
-    v_topics = topic_model.visualize_topics()
-    v_distribution = topic_model.visualize_distribution(
-        probabilities[0], min_probability=0.015
-    )
 
-    if not VISUALIZATION_DIR.exists():
-        VISUALIZATION_DIR.mkdir(parents=True, exist_ok=True)
+    # v_topics = topic_model.visualize_topics()
+    # v_distribution = topic_model.visualize_distribution(
+    #     probabilities[0], min_probability=0.015
+    # )
 
-    v_topics.write_html(VISUALIZATION_DIR / "topics.html")
-    v_distribution.write_html(VISUALIZATION_DIR / "distribution.html")
+    # if not VISUALIZATION_DIR.exists():
+    #     VISUALIZATION_DIR.mkdir(parents=True, exist_ok=True)
+
+    # v_topics.write_html(VISUALIZATION_DIR / "topics.html")
+    # v_distribution.write_html(VISUALIZATION_DIR / "distribution.html")
 
     for i, article in enumerate(articles):
         article["cluster_id"] = int(topics[i])
@@ -109,11 +108,11 @@ def save_grouped_articles(articles, output_file):
     # Save grouped articles to a JSON file and return grouped data
     grouped_articles = {}
     for article in articles:
-        cluster_id = article["cluster_id"]
+        cluster_id = article["cluster_id"] 
         grouped_articles.setdefault(cluster_id, []).append(article)
 
     with open(output_file, "w", encoding="utf-8") as f:
-        json.dump(grouped_articles, f, indent=2)
+        json.dump(grouped_articles, f, indent=4)
 
     return grouped_articles
 
