c5f6e8108efa826649c33db2f68447d2a630db7b
Curstantine <curstantine@proton.me>
2025-03-10 17:24:12 +0530

    fix: model transformations, topic sizes and outlet names

--- src/app/api/sync/grouping.py
+++ src/app/api/sync/grouping.py
@@ -1,7 +1,6 @@
 # Import necessary libraries
 import os
 import json
-import numpy as np
 from pathlib import Path
 from bertopic import BERTopic
 from sentence_transformers import SentenceTransformer
@@ -11,6 +10,37 @@ REPO_ROOT = Path(os.getcwd())
 ARTICLES_DIR = REPO_ROOT / "news_filtered_data/news_source_data/data/articles"
 OUTPUT_FILE = REPO_ROOT / "news_filtered_data/clustered.json"
 
+MODEL_DIR = REPO_ROOT / "news_filtered_data/clustered_model"
+VISUALIZATION_DIR = REPO_ROOT / "news_filtered_data/visualization"
+
+
+def get_outlet_name(url):
+    domain = url.split("/")[2]
+    match domain:
+        case "www.ft.lk":
+            return "Daily FT"
+        case "www.newsfirst.lk":
+            return "News First"
+        case "www.adaderana.lk":
+            return "Ada Derana"
+        case "www.news.lk":
+            return "News.lk"
+        case "dbsjeyaraj.com":
+            return "DBS Jeyaraj"
+        case "www.dailymirror.lk":
+            return "Daily Mirror"
+        case "www.dailynews.lk":
+            return "Daily News"
+        case "www.island.lk":
+            return "The Island"
+        case "www.sundaytimes.lk":
+            return "Sunday Times"
+        case "www.dailypioneer.com":
+            return "Daily Pioneer"
+        case _:
+            print(f"[cluster] Unknown outlet: {domain}")
+            return "unknown"
+
 
 def load_articles(directory):
     articles = []
@@ -19,6 +49,10 @@ def load_articles(directory):
         with open(file, "r", encoding="utf-8") as f:
             data = json.load(f)
             text = " ".join(data.get("body_paragraphs", [])).strip()
+            outlet = get_outlet_name(data["url"])
+
+            if outlet == "unknown" or not text:
+                continue
 
             articles.append(
                 {
@@ -26,26 +60,44 @@ def load_articles(directory):
                     "title": data["title"],
                     "ut": data["ut"],
                     "body_paragraphs": text,
-                    "outlet": data.get("outlet", "unknown"),
+                    "outlet": outlet,
                     "reporter": data.get("reporter", None),
                 }
             )
 
+    print(f"[cluster] Found {len(articles)} articles for clustering.")
     return articles
 
 
-def cluster_articles(articles, num_clusters=5):
+def cluster_articles(articles):
     # Cluster articles using BERTopic
     body_paragraphs = [article["body_paragraphs"] for article in articles]
 
     if not any(body_paragraphs):
         raise ValueError("No valid articles found for clustering.")
 
-    model = SentenceTransformer("all-MiniLM-L6-v2")
-    embeddings = model.encode(body_paragraphs, show_progress_bar=True)
+    model = SentenceTransformer("paraphrase-MiniLM-L12-v2")
 
-    topic_model = BERTopic(nr_topics=num_clusters)
-    topics, _ = topic_model.fit_transform(body_paragraphs, embeddings)
+    topic_model = BERTopic(
+        language="english",
+        calculate_probabilities=True,
+        verbose=True,
+        embedding_model=model,
+        nr_topics=64,
+    )
+    topics, probabilities = topic_model.fit_transform(body_paragraphs)
+    topic_model.save(MODEL_DIR, serialization="safetensors", save_ctfidf=True)
+
+    v_topics = topic_model.visualize_topics()
+    v_distribution = topic_model.visualize_distribution(
+        probabilities[0], min_probability=0.015
+    )
+
+    if not VISUALIZATION_DIR.exists():
+        VISUALIZATION_DIR.mkdir(parents=True, exist_ok=True)
+
+    v_topics.write_html(VISUALIZATION_DIR / "topics.html")
+    v_distribution.write_html(VISUALIZATION_DIR / "distribution.html")
 
     for i, article in enumerate(articles):
         article["cluster_id"] = int(topics[i])
@@ -67,12 +119,13 @@ def save_grouped_articles(articles, output_file):
 
 
 def create_reporter_and_outlet(article):
-    # Create reporter and outlet information
-    reporter = article["reporter"] or f"system-{article['outlet']}"
+    outlet = get_outlet_name(article["url"])
+    reporter = article["reporter"] or f"system-{outlet.lower().split(" ")[0].join('_')}"
+
     return {
+        "outlet": outlet,
         "reporter": reporter,
         "is_system": article["reporter"] is None,
-        "outlet": article["outlet"],
     }
 
 
@@ -86,9 +139,7 @@ if __name__ == "__main__":
     articles = load_articles(ARTICLES_DIR)
 
     if articles:
-        clustered_articles = cluster_articles(
-            articles, num_clusters=min(5, len(articles))
-        )
+        clustered_articles = cluster_articles(articles)
         print("[cluster] Clustering Completed.")
 
         processed_articles = process_articles(clustered_articles)
