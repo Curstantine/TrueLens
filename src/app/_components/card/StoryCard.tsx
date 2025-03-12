996f11f5ae1ac08f6ca81647488ae3b0ab094ef1
Curstantine <curstantine@proton.me>
2025-03-12 23:09:17 +0530

    fix: remove placeholder cover in covercard

--- src/app/_components/card/StoryCard.tsx
+++ src/app/_components/card/StoryCard.tsx
@@ -14,8 +14,7 @@ export default function StoryCard({ id, title, cover, noOfArticles, factuality }
 		<Link
 			draggable={false}
 			href={`/story/${id}`}
-			className="grid grid-cols-[1fr_--spacing(24)] items-center gap-4 rounded-md bg-background border border-border pl-3"
-
+			className="grid grid-cols-[1fr_--spacing(24)] items-center gap-4 rounded-md border border-border bg-background pl-3"
 		>
 			<div className="flex flex-grow flex-col gap-1">
 				<span className="leading-tight font-semibold text-foreground">{title}</span>
@@ -26,7 +25,7 @@ export default function StoryCard({ id, title, cover, noOfArticles, factuality }
 				</div>
 			</div>
 
-			{cover ? (
+			{cover && (
 				<Image
 					src={cover}
 					alt="Story Image"
@@ -37,8 +36,6 @@ export default function StoryCard({ id, title, cover, noOfArticles, factuality }
 					className="size-24 rounded-r-md object-cover"
 					unoptimized
 				/>
-			) : (
-				<div className="h-24 w-24 rounded-r-md bg-muted"></div>
 			)}
 		</Link>
 	);
