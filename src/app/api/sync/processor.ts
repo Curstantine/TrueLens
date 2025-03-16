eed32d6db7a18ee8985b63999cf65f972acf8499
Steph-kushi12 <kushistephanie@gmail.com>
2025-03-16 18:29:55 +0530

    Added factuality analysis to article processing

--- src/app/api/sync/processor.ts
+++ src/app/api/sync/processor.ts
@@ -4,9 +4,7 @@ import { createStory, createArticle } from "./article";
 
 export async function processArticlesAndGenerateReport(articles: any[]) {
     const normalizedSummary = generateNormalizedSummary(articles.map(article => article.content));
-
     const factualityReport = await calculateFactuality(articles);
-
     const story = await createStory("News Story Title", "This is the summary of the news story.");
 
     for (const article of articles) {
