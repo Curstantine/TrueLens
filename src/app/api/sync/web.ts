c9c2cc53785e4e2a2cf92f06145fda7710e20566
Kirushnabalan <kirushnabalan1803@gmail.com>
2025-03-20 15:43:12 +0530

    try other tags for scraping image fom ft.lk link

--- src/app/api/sync/web.ts
+++ src/app/api/sync/web.ts
@@ -54,7 +54,7 @@ export class WebScraper {
 
       // Scrape from FT.lk
       if (url.includes('ft.lk')) {
-        newsImage = $("div.article-content img").first().attr("src");
+        newsImage = $("p> img").first().attr("src");
       }
       // Scrape from Daily Mirror
       else if (url.includes('dailymirror.lk')) {
