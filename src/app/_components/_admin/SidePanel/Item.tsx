0295dbb5d7300292a7ed52265b0174e54719c101
Curstantine <curstantine@proton.me>
2025-03-22 16:57:29 +0530

    feat: implement sidebar with links

---
+++ src/app/_components/_admin/SidePanel/Item.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import clsx from "clsx/lite";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import type { ReactNode } from "react";
+
+type ItemProps = { href: string; label: string; icon: ReactNode };
+
+export default function AdminSidePanelItem({ href, label, icon }: ItemProps) {
+	const pathname = usePathname();
+
+	return (
+		<Link
+			href={href}
+			className={clsx(
+				"inline-flex h-8 w-full items-center gap-2 rounded-md px-2 hover:bg-muted/30 active:bg-muted/50",
+				pathname === href && "bg-muted",
+			)}
+		>
+			{icon}
+			{label}
+		</Link>
+	);
+}
