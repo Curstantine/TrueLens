d77c8b373875bf91a959f730ad4d8ecb43febdc8
Curstantine <curstantine@proton.me>
2025-04-02 12:25:47 +0530

    debug: remove action logging

--- src/server/sync/scraper.ts
+++ src/server/sync/scraper.ts
@@ -37,7 +37,6 @@ export async function scrapeDeranaHotNews(page = 1): Promise<SourceArticle[]> {
 	const articles: SourceArticle[] = [];
 	const jobs: Promise<void>[] = [];
 
-	console.log(html);
 	for (let i = 0; i < divs.length; i++) {
 		const article = $(divs[i]);
 		const header = article.find("h2.hidden-xs > a[target='_blank']");
