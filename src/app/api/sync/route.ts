88ffbcf3607aa516b57062841eae172a3e2e6a09
Curstantine <curstantine@proton.me>
2025-04-05 23:49:54 +0530

    refactor: change auth wrapper to session getter, move ai shenanigans to ai file

--- src/app/api/sync/route.ts
+++ src/app/api/sync/route.ts
@@ -12,21 +12,14 @@ import { UserRole, type NewsOutlet, type Reporter } from "@prisma/client";
 import { api } from "~/trpc/server";
 import { auth } from "~/server/auth";
 import { runScraper } from "~/server/sync/scraper";
+import { factualize, summarize } from "~/server/ai";
 import { getCoverImage, getSiteFavicon } from "~/server/sync/web";
-import { factCheckingModel, summarizationModel } from "~/server/ai";
 import { isMostlyEnglish, readClusteredArticles } from "~/server/sync/utils";
 import type {
 	ClusteredSourceArticles,
 	ClusteredSummaryFactualityReport,
-	SourceArticle,
 	SummarizedArticle,
 } from "~/server/sync/types";
-import {
-	type ArticleSummary,
-	type StoryFactualityReport,
-	ARTICLE_SUMMARY,
-	STORY_FACTUALITY_REPORT,
-} from "~/server/sync/models";
 
 export const runtime = "nodejs";
 
@@ -36,8 +29,9 @@ const CLUSTERED_FILE = joinPath(DATA_FOLDER, "clustered.json");
 
 const log = (...msg: string[]) => console.log(`[sync]`, ...msg);
 
-export const POST = auth(async function POST(req) {
-	if (req.auth?.user.role !== UserRole.ADMIN) {
+export async function POST() {
+	const session = await auth();
+	if (session?.user.role !== UserRole.ADMIN) {
 		return NextResponse.json({ status: "error", message: "Unauthorized" }, { status: 401 });
 	}
 
@@ -228,47 +222,6 @@ export const POST = auth(async function POST(req) {
 	await api.configuration.updateLastSync({ value: new Date().toISOString() });
 
 	return NextResponse.json({ status: "ok" });
-});
-
-async function summarize(article: SourceArticle): Promise<ArticleSummary> {
-	const model = summarizationModel()
-		.withStructuredOutput(ARTICLE_SUMMARY)
-		.withRetry({ stopAfterAttempt: 4 });
-	const prompt = `Return a summary of the news article in a readable point format. Try not to span to more than 6 points.
-	Return a valid JSON following the example format: {{ "summary": ["point 1", "point 2", "point 3"] }}`;
-
-	return await model.invoke([
-		{ role: "system", content: prompt },
-		{ role: "user", content: article.body.join("\n") },
-	]);
-}
-
-async function factualize(articles: SummarizedArticle[]): Promise<StoryFactualityReport> {
-	const model = factCheckingModel()
-		.withStructuredOutput(STORY_FACTUALITY_REPORT)
-		.withRetry({ stopAfterAttempt: 2 });
-	const prompt = `Return a factuality report from each outlet. The factuality is calculated by averaging what has happened in each article.
-	The data must be returned in JSON format, paired by the temp_id, which should not be changed as they are used to identify the articles.
-	Factuality is a float between 0 and 1, where 0 is completely false and 1 is completely true.
-
-	EXAMPLE OUTPUT:
-	{
-		data: [
-			{ temp_id: 'temp_id_1', factuality: 0.8 },
-			{ temp_id: 'temp_id_2', factuality: 0.6 },
-			{ temp_id: 'temp_id_3', factuality: 0.4 },
-		]
-	}`;
-
-	return await model.invoke([
-		{ role: "system", content: prompt },
-		{
-			role: "user",
-			content: articles
-				.map((x) => `temp_id: ${x.temp_id}\nSummary: ${x.summary.join(" ")}`)
-				.join("\n\n"),
-		},
-	]);
 }
 
 async function getOrCreateOutlet(
--- src/server/ai/index.ts
+++ src/server/ai/index.ts
@@ -2,8 +2,15 @@ import { cache } from "react";
 import { ChatGroq } from "@langchain/groq";
 
 import { env } from "~/env";
+import type { SourceArticle, SummarizedArticle } from "~/server/sync/types";
+import {
+	ARTICLE_SUMMARY,
+	STORY_FACTUALITY_REPORT,
+	type ArticleSummary,
+	type StoryFactualityReport,
+} from "~/server/sync/models";
 
-export const summarizationModel = cache(
+const summarizationModel = cache(
 	() =>
 		new ChatGroq({
 			model: "llama-3.2-3b-preview",
@@ -12,7 +19,7 @@ export const summarizationModel = cache(
 		}),
 );
 
-export const factCheckingModel = cache(
+const factCheckingModel = cache(
 	() =>
 		new ChatGroq({
 			model: "llama-3.3-70b-versatile",
@@ -20,3 +27,44 @@ export const factCheckingModel = cache(
 			apiKey: env.GROQ_API_KEY,
 		}),
 );
+
+export async function summarize(article: SourceArticle): Promise<ArticleSummary> {
+	const model = summarizationModel()
+		.withStructuredOutput(ARTICLE_SUMMARY)
+		.withRetry({ stopAfterAttempt: 4 });
+	const prompt = `Return a summary of the news article in a readable point format. Try not to span to more than 6 points.
+	Return a valid JSON following the example format: {{ "summary": ["point 1", "point 2", "point 3"] }}`;
+
+	return await model.invoke([
+		{ role: "system", content: prompt },
+		{ role: "user", content: article.body.join("\n") },
+	]);
+}
+
+export async function factualize(articles: SummarizedArticle[]): Promise<StoryFactualityReport> {
+	const model = factCheckingModel()
+		.withStructuredOutput(STORY_FACTUALITY_REPORT)
+		.withRetry({ stopAfterAttempt: 2 });
+	const prompt = `Return a factuality report from each outlet. The factuality is calculated by averaging what has happened in each article.
+	The data must be returned in JSON format, paired by the temp_id, which should not be changed as they are used to identify the articles.
+	Factuality is a float between 0 and 1, where 0 is completely false and 1 is completely true.
+
+	EXAMPLE OUTPUT:
+	{
+		data: [
+			{ temp_id: 'temp_id_1', factuality: 0.8 },
+			{ temp_id: 'temp_id_2', factuality: 0.6 },
+			{ temp_id: 'temp_id_3', factuality: 0.4 },
+		]
+	}`;
+
+	return await model.invoke([
+		{ role: "system", content: prompt },
+		{
+			role: "user",
+			content: articles
+				.map((x) => `temp_id: ${x.temp_id}\nSummary: ${x.summary.join(" ")}`)
+				.join("\n\n"),
+		},
+	]);
+}
